5c6d8b06933c18e7be60f16e2dfa5898eb99ebab
Nahuel Krowicki <nkrowicki@nubedi.com>
2020-09-25 01:08:43 -0300

    First commit by Nahuel Krowicki!

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './App.module.scss';
+console.log(styles)
+const App = () => {
+    return (
+        <div className={styles.app}>
+            <h1>Hola React</h1>
+        </div>
+    )
+}
+
+export default App
